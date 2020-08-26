60928a54a6ccaa71c542d89938578f005fd9fc49
Straw-b <1609332043@qq.com>
2020-08-26 11:09:05 +0800

    类的定义，封装

---
+++ test0730/test0730/Person.h
@@ -0,0 +1,16 @@
+//
+//
+//class Person
+//{
+//	// Person具有其相应的功能--->成员函数
+//	void SetPersonInfo(char name[], char gender[], int age);
+//	void PrintStudentInfo();
+//	void Eat();
+//	void Sleep();
+//	void Work();
+//
+//	// 用来描述人的属性--->成员变量
+//	char _name[20];
+//	char _gender[3];
+//	int _age;
+//};
